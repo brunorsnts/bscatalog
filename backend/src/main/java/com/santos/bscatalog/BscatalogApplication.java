7a665d2fff58da682b9ad71004f6b8885a9b9a99
brunorsnts <santos_2002@outlook.com.br>
2025-09-26 15:09:50 -0300

    Backend created

---
+++ backend/src/main/java/com/santos/bscatalog/BscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.santos.bscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/santos/bscatalog/BscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.bscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
